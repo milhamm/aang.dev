5353bf9a7017c6e10bf24fb8e0693831b3c8c36b
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-11-29 00:06:06 +0700

    fix: change next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production' || process.env.IS_VERCEL_ENV === 'true'
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  compiler: {
-    removeConsole: isProduction,
-  },
-  eslint: {
-    ignoreDuringBuilds: isProduction,
-  },
-  typescript: {
-    ignoreBuildErrors: isProduction,
-  },
   images: {
     domains: ['cdn.discordapp.com', 'avatars.githubusercontent.com'],
   },
