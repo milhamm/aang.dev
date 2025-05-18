d52cf7c61838f7cf9277e7b8b154ea1b73eedf2e
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2025-05-18 19:43:34 +0700

    feat: v2 checkpoint

--- aang.dev/apps/personal/app/routes.tsx
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes';
-
-export default [
-  index('./app.tsx'),
-  route('about', './routes/about.tsx'),
-] satisfies RouteConfig;
